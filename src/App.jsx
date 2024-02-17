55af25224563cb4905b54f3cd4a839da345d2088
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-02-17 18:11:05 +0500

    get current location

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const tg = window.Telegram.WebApp;
 
+const map = L.map("map").setView([0, 0], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+map.locate({ setView: true, maxZoom: 16 });
+
+function onLocationFound(e) {
+	var radius = e.accuracy;
+
+	L.marker(e.latlng)
+		.addTo(map)
+		.bindPopup("You are within " + radius + " meters from this point")
+		.openPopup();
+
+	L.circle(e.latlng, radius).addTo(map);
+}
+
+navigator.geolocation.getCurrentPosition(
+	(position) => map.panTo([position.coords.latitude, position.coords.longitude]),
+	(error) => {
+		alert(error);
+	}
+);
+
+map.on("locationfound", onLocationFound);
+var marker = L.marker([41, 63]).addTo(map);
 function App() {
-	const handleClick = () => {
-		tg.close();
-	};
+	const [location, setLocation] = useState([]);
 
-	useEffect(() => {
-		tg.ready();
-	}, []);
-
-	return (
-		<>
-			<h1 className="text-red-500 text-5xl text-center">Hello world</h1>
-			<button onClick={handleClick}>Close window</button>
-		</>
-	);
+	return <></>;
 }
 
 export default App;
