a5c77af081a2a5a375de4fe78fa457c2aa83b1e9
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-02-24 21:26:35 +0500

    test for view on phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "./App.css";
 import { Map } from "./Components/Map/Map";
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 
-const socket = io("ws://localhost:3000");
+// const socket = io("ws://localhost:3000");
 
-let uniqueId;
+// let uniqueId;
 
-socket.on("clientJoined", async (data) => {
-	uniqueId = await data;
-	console.log(uniqueId);
-});
+// socket.on("clientJoined", async (data) => {
+//   uniqueId = await data;
+//   console.log(uniqueId);
+// });
 
-const tg = window.Telegram.WebApp;
+// const tg = window.Telegram.WebApp;
 
-socket.on("connected", (data) => {
-	console.log(data);
-});
+// socket.on("connected", (data) => {
+//   console.log(data);
+// });
 
 // tg.on("location", (data) => {
 // 	console.log(data);
 // });
 
 function App() {
-	return <Map />;
+  return <Map />;
 }
 
 export default App;
--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,69 +1,76 @@
 import axios from "axios";
-import { map } from "leaflet";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
 
 function LocationMarker() {
-	const [position, setPosition] = useState(null);
+  const [position, setPosition] = useState(null);
 
-	const map = useMapEvents({
-		dblclick() {
-			map.locate();
-		},
-		locationfound(e) {
-			map.flyTo(e.latlng, map.getZoom());
-		},
-		move() {
-			setPosition(map.getCenter());
-		},
-		moveend() {
-			axios(
-				`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${map.getCenter().lat}&lon=${
-					map.getCenter().lng
-				}`
-			).then(function ({ data }) {
-				console.log(data.address.road);
-				console.log(data.display_name.split(", ", 2));
-			});
-		},
-	});
-	map.setMaxZoom(18);
-	map.setMinZoom(11);
+  const map = useMapEvents({
+    dblclick() {
+      map.locate();
+    },
+    locationfound(e) {
+      map.flyTo(e.latlng, map.getZoom());
+    },
+    // move() {
+    //   setPosition(map.getCenter());
+    // },
+    moveend() {
+      axios(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${map.getCenter().lat}&lon=${map.getCenter().lng}`).then(function ({ data }) {
+        console.log(data.address.road);
+        console.log(data.display_name.split(", ", 2));
+      });
+    },
+  });
+  map.setMaxZoom(18);
+  map.setMinZoom(11);
 
-	return position === null ? null : (
-		<Marker position={position}>
-			<Popup>You are here</Popup>
-		</Marker>
-	);
+  navigator.geolocation.getCurrentPosition((currentLocation) => {
+    setPosition({ lat: currentLocation.coords.latitude, lng: currentLocation.coords.longitude });
+    // map.locate();
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
 }
 
-function Component() {
-	const map = useMapEvents({
-		// click() {
-		// 	map.locate();
-		// },
-	});
-	return <button onClick={() => map.locate()}>Locate</button>;
-}
+// function Component() {
+//   const map = useMapEvents({
+// click() {
+// 	map.locate();
+// },
+//   });
+//   return <button onClick={() => map.locate()}>Locate</button>;
+// }
 
 export const Map = () => {
-	return (
-		<MapContainer
-			className="w-screen h-screen"
-			center={{ lat: 41.347093840376516, lng: 69.22704219818117 }}
-			zoom={13}
-			scrollWheelZoom={false}>
-			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			<LocationMarker />
+  const [position, setPosition] = useState(null);
 
-			{/* <div className="bg-red-500 z-[1000] absolute ">
+  // const map = useMapEvents({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((currentLocation) => {
+      setPosition({ lat: currentLocation.coords.latitude, lng: currentLocation.coords.longitude });
+    });
+  }, []);
+  console.log(1);
+  return (
+    <>
+      {position && (
+        <MapContainer className="w-screen h-screen" center={position} zoom={10} scrollWheelZoom={true}>
+          <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <LocationMarker />
+
+          {/* <div className="bg-red-500 z-[1000] absolute ">
 				<Component />
 				<div className="w-48 h-80"></div>
 				<div className=" hover:scale-95 text-3xl bg-gray-500 active:bg-yellow-300 w-10 h-10"></div>
 			</div> */}
-		</MapContainer>
-	);
+        </MapContainer>
+      )}
+    </>
+  );
 };
